ef0cf702667988496fdfea12366ee14129a52eff
IsraelaZimru <slzimro@gmail.com>
2023-01-19 11:32:51 +0200

    adding navbar with styling library - bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import ListGroup from "react-bootstrap/ListGroup";
+import "./App.css";
+import PageData from "../src/data/Legends.json";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [show, setShow] = useState(false);
 
+  const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
   return (
     <div className="App">
-     lalalalal
+      <Nav fill variant="tabs" onSelect={handleSelect}>
+        {!!PageData &&
+          PageData.MonitorType.map((item, i) => {
+            const MonitorTypeId = item.Id;
+            // console.log(
+            //   "{item.name",
+            //   item,
+            //   "MonitorTypeId",
+            //   MonitorTypeId,
+            //   PageData.Monitor.filter((x) => x.MonitorTypeId === MonitorTypeId)
+            // );
+            return (
+              <NavDropdown title={item.Name}>
+                {PageData.Monitor.filter(
+                  (x) => x.MonitorTypeId === MonitorTypeId
+                ).map((link, i) => (
+                  <NavDropdown.Item eventKey={item.LegendId}>
+                    {link.Name}
+                  </NavDropdown.Item>
+                ))}
+              </NavDropdown>
+            );
+          })}
+      </Nav>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
