1ecfa94f2ea27c54d601434daca10114d7dc9b56
IsraelaZimru <slzimro@gmail.com>
2023-01-19 12:20:45 +0200

    styling legend info section

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,28 @@ import PageData from "../src/data/Legends.json";
 
 function App() {
   const [show, setShow] = useState(false);
+  const [selectedLegend, setSelectedLegend] = useState(PageData.Legends[1]);
 
-  const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
+  const handleSelect = (id) => {
+    const legend = PageData.Legends.find((x) => x.Id === id);
+    console.log("legend", legend);
+    setSelectedLegend(legend);
+    setShow(true);
+  };
+
+  const closeLegend = () => setShow(false);
   return (
     <div className="App">
-      <Nav fill variant="tabs" onSelect={handleSelect}>
+      <Nav fill variant="tabs">
         {!!PageData &&
           PageData.MonitorType.map((item, i) => {
             const MonitorTypeId = item.Id;
-            // console.log(
-            //   "{item.name",
-            //   item,
-            //   "MonitorTypeId",
-            //   MonitorTypeId,
-            //   PageData.Monitor.filter((x) => x.MonitorTypeId === MonitorTypeId)
-            // );
             return (
               <NavDropdown title={item.Name}>
                 {PageData.Monitor.filter(
                   (x) => x.MonitorTypeId === MonitorTypeId
                 ).map((link, i) => (
-                  <NavDropdown.Item eventKey={item.LegendId}>
+                  <NavDropdown.Item onClick={() => handleSelect(item.LegendId)}>
                     {link.Name}
                   </NavDropdown.Item>
                 ))}
@@ -36,6 +37,45 @@ function App() {
             );
           })}
       </Nav>
+      {show && (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <div
+            style={{
+              border: " 2px black solid",
+              textAlign: "left",
+              marginTop: "100px",
+              padding: "20px 60px",
+              alignSelf: "center",
+              width: "40%",
+            }}
+          >
+            {/* CLOSE BUTTON */}
+            <p
+              style={{ textAlign: "right", fontWeight: "bolder" }}
+              onClick={closeLegend}
+            >
+              X
+            </p>
+            {selectedLegend.tags.map((tag) => (
+              <p>
+                <span
+                  style={{
+                    padding: "4px 20px",
+                    marginRight: "10px",
+                    backgroundColor: tag.Color,
+                  }}
+                ></span>
+                {tag.Label}
+              </p>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
